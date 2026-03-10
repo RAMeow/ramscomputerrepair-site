00ded47376bef696d7e8af5e04742cbb0d6cb43c
RAMeow <86502704+RAMeow@users.noreply.github.com>
2026-03-10 00:39:00 -0500

    Delete index.tsx

--- index.tsx
+++
@@ -1,7 +0,0 @@
-// See canvas document Rams-computer-repair-refresh
-// This file represents the latest React site build with RAMeow portal route.
-// Copy the full canvas code here if editing locally.
-
-export default function RamsComputerRepairRefresh() {
-  return null;
-}
