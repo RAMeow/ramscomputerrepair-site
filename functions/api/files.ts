a7f81618ba2bed6e3afd27f8c0bfea76999c83c3
RAMeow <86502704+RAMeow@users.noreply.github.com>
2026-03-10 04:10:36 -0500

    Update files.ts

--- functions/api/files.ts
+++ functions/api/files.ts
@@ -1,1 +1,18 @@
+export const onRequestGet: PagesFunction<{
+  RAMEOW_BUCKET: R2Bucket;
+}> = async ({ env }) => {
+  const listed = await env.RAMEOW_BUCKET.list();
 
+  const files = listed.objects.map((obj) => ({
+    key: obj.key,
+    size: obj.size,
+    uploaded: obj.uploaded.toISOString(),
+  }));
+
+  return new Response(JSON.stringify({ files }), {
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store",
+    },
+  });
+};
